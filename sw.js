c3ea4fcd623604636092a8bcb7a0c752ffac3a6e
Naman Singh <snaman431@gmail.com>
2021-09-28 14:33:05 +0530

    Converted to PWA

--- sw.js
+++ sw.js
@@ -1,55 +1,48 @@
-// const staticAssest = [
-//     './',
-//     '/img/cookingOil.jpg',
-//     '/img/coriander.jpg',
-//     '/img/flour.jpg',
-//     '/img/jam.jpg',
-//     '/img/onions.jpg',
-//     '/img/potato.jpg',
-//     '/img/salt.jpg',
-//     '/img/soup.jpg',
-//     '/index.css',
-//     '/index.js',
-//     '/manifest.json',
-//     '/public/img/gross.png'
-// ];
+const staticAssest = [
+    './',
+    '/index.css',
+    '/index.js',
+    '/manifest.json',
+    '/public/img/gross.png'
+];
 
 
-// self.addEventListener('install',async evt=>{
-//     const cache = await caches.open('gross-static');
-//     cache.addAll(staticAssest);
-// });
+self.addEventListener('install',async evt=>{
+    const cache = await caches.open('gross-static');
+    cache.addAll(staticAssest);
+});
 
-// self.addEventListener('activate',evt=>{
-//     console.log("Activate");
-// });
+self.addEventListener('activate',evt=>{
+    console.log("Activate");
+});
 
-// self.addEventListener('fetch',event=>{
-//     const req = event.request;
-//     const url = new URL(req.url);
-//     if(url.origin ===location.origin)
-//     {
-//         event.respondWith(cacheFirst(req));
-//     }
-//     else
-//     {
-//         event.respondWith(networkFirst(req));
-//     }
-// });
+self.addEventListener('fetch',event=>{
+    const req = event.request;
+    const url = new URL(req.url);
+    //console.log(url);
+    if(url.origin === location.origin)
+    {
+        event.respondWith(cacheFirst(req));
+    }
+    else
+    {
+        event.respondWith(networkFirst(req));
+    }
+});
 
-// async function cacheFirst(req){
-//     const cacheResponse = await caches.match(req);
-//     return cacheResponse || fetch(req);
-// }
+async function cacheFirst(req){
+    const cacheResponse = await caches.match(req);
+    return cacheResponse || fetch(req);
+}
 
-// async function cacheFirst(req){
-//     const cache = await caches.open('gross-dynamic');
-//     try{
-//         const res = await fetch(req);
-//         cache.put(req,res.clone());
-//         return res;
-//     }catch(error)
-//     {
-//         return await cache.match(req);
-//     }
-// }
+async function networkFirst(req){
+    const cache = await caches.open('gross-dynamic');
+    try{
+        const res = await fetch(req);
+        cache.put(req,res.clone());
+        return res;
+    }catch(error)
+    {
+        return await cache.match(req);
+    }
+}
